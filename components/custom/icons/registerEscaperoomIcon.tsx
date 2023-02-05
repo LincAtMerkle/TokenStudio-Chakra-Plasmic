dfb5b1c1c22776ceb75ffb1c841bc1a69097e172
Lincoln Mitchell <lincolnmitchell@merkle.com>
2023-02-06 07:45:00 +1100

    added custom component registrations

--- components/custom/icons/registerEscaperoomIcon.tsx
+++ components/custom/icons/registerEscaperoomIcon.tsx
@@ -1,31 +1,31 @@
-import { IconProps } from "@chakra-ui/icons";
-import { ComponentMeta } from "@plasmicapp/host/registerComponent";
-import {
-  getComponentNameAndImportMeta,
-  getPlasmicComponentName,
-} from "../chakra/utils";
-export const escaperoomIconMeta: ComponentMeta<IconProps> = {
-  ...getComponentNameAndImportMeta("EscaperoomIcon"),
-  props: {
-    viewBox: {
-      type: "string",
-      defaultValue: "0 0 32 32",
+import { EscaperoomIcon } from "./CustomIcons";
+import { registerComponent } from "@plasmicapp/host";
+
+export function registerEscaperoomIcon() {
+  registerComponent(EscaperoomIcon, {
+    name: "EscaperoomIcon",
+    importPath: "@chakra-ui/react",
+    props: {
+      viewBox: {
+        type: "string",
+        defaultValue: "0 0 32 32",
+      },
+      boxSize: {
+        type: "string",
+        defaultValue: "1em",
+      },
+      color: {
+        type: "string",
+        defaultValue: "currentColor",
+      },
+      focusable: {
+        type: "boolean",
+        defaultValue: false,
+      },
+      role: {
+        type: "choice",
+        options: ["presentation", "img"],
+      },
     },
-    boxSize: {
-      type: "string",
-      defaultValue: "1em",
-    },
-    color: {
-      type: "string",
-      defaultValue: "currentColor",
-    },
-    focusable: {
-      type: "boolean",
-      defaultValue: false,
-    },
-    role: {
-      type: "choice",
-      options: ["presentation", "img"],
-    },
-  },
-};
+  });
+}
