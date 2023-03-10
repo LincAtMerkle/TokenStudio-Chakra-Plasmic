02ed251301d3f6273c3823d3c396f45935c361bb
Lincoln Mitchell <lincolnmitchell@merkle.com>
2023-03-10 17:12:11 +1100

    fri eve push for Sami

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,28 @@
+import React from 'react';
+
+import { ChakraProvider, ChakraProviderProps } from "@chakra-ui/react";
+import { theme as proTheme } from "../theme/src/index";
+import { extendTheme, theme as baseTheme } from "@chakra-ui/react";
+
+const theme = extendTheme(
+    {
+      colors: { ...baseTheme.colors, brand: baseTheme.colors.yellow },
+    },
+    proTheme
+  );
+  console.log(baseTheme)
+  
+export const decorators = [
+  (Story) => (
+    
+    <ChakraProvider theme={theme}>
+      <Story />
+    </ChakraProvider>
+  ),
+];
+
+
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +31,35 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  chakra: {
+    theme:{theme: proTheme},
+  },
 }
+
+// export const decorators = [
+//   (Story) => {
+//     globalCss({
+//       // "@import": "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap",
+//       "*": {
+//         boxSizing: "border-box",
+//         "&:focus": {
+//           outline: "1px dashed black",
+//           outlineOffset: "1px"
+//         }
+//       },
+//       body: {
+//         fontFamily: "$body",
+//       }
+//     })()
+
+//     return (
+//     <ChakraProvider theme={theme}>
+//       <ThemeProvider>
+//         <InterfaceIconsProvider>
+//           <Story />
+//         </InterfaceIconsProvider>
+//       </ThemeProvider>
+//     </ChakraProvider>
+//     )
+//   },
+// ]
