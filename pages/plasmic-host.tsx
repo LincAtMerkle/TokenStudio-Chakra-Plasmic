5877ddacffdcc71fd4d77d9d9fe6e29825140d07
Lincoln Mitchell <lincolnmitchell@merkle.com>
2023-03-25 10:40:18 +1100

    added tokens

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -4,6 +4,8 @@ import Script from 'next/script';
 import { PlasmicCanvasHost, registerComponent, registerToken } from '@plasmicapp/host';
 import { registerAllChakra } from "../components/chakra";
 import { registerAllCustom } from "../components/custom";
+import  semanticTokens from "../theme/src/foundations/tokens"
+
 
 // You can register any code components that you want to use here; see
 // https://docs.plasmic.app/learn/code-components-ref/
@@ -21,28 +23,8 @@ export default function PlasmicHost() {
 registerAllChakra();
 registerAllCustom();
 
-registerToken({
-  type: "color",
-  name: "Token from code",
-  value: "#F123CC"
-})
 
-const tokenStudio = {
-  "colors": {
-    "st-dark": {
-      "color-fl-green-50": "#fcfff6",
-      "color-fl-green-100": "#f2ffdc",
-      "color-fl-green-200": "#e6ffb8",
-    },
-    "st-light": {
-      "color-fl-green-50": "#fcfff6",
-      "color-fl-green-100": "#f2ffdc",
-      "color-fl-green-200": "#e6ffb8",
-    }
-  }
-}
-
-Object.entries(tokenStudio.colors)
+Object.entries(semanticTokens.colors)
   .flatMap(([colorName, colorOptions]) =>
     Object.entries(colorOptions).map(([colorLevel, colorValue]) => ({
       name: `${colorName} ${colorLevel}`,
@@ -56,21 +38,110 @@ Object.entries(tokenStudio.colors)
     })
   })
 
-// same as above
-// for (const [colorGroupName, colorGroup] of Object.entries(tokenStudio.colors)) {
-//   for (const [colorLevelName, colorValue] of Object.entries(colorGroup)) {
-//     registerToken({
-//       type: "color",
-//       value: colorValue,
-//       name: `${colorGroupName} ${colorLevelName}`
-//     })
-//   }
-// }
 
-registerToken({
-  type: "opacity",
-  name: "opa token",
-  value: "0.5"
-})
+  Object.entries(semanticTokens.space)
+  .flatMap(([spaceName, spaceOptions]) =>
+    Object.entries(spaceOptions).map(([spaceLevel, spaceValue]) => ({
+      name: `${spaceName} ${spaceLevel}`,
+      value: spaceValue
+    })
+  )).forEach(({ name, value}) => {
+    registerToken({
+      type: "spacing",
+      name,
+      value
+    })
+  })
+
+  Object.entries(semanticTokens.fonts)
+  .flatMap(([fontsName, fontsOptions]) =>
+    Object.entries(fontsOptions).map(([fontsLevel, fontsValue]) => ({
+      name: `${fontsName} ${fontsLevel}`,
+      value: fontsValue
+    })
+  )).forEach(({ name, value}) => {
+    registerToken({
+      type: "font-family",
+      name,
+      value
+    })
+  })
+
+  Object.entries(semanticTokens.fontSizes)
+  .flatMap(([fontSizeName, fontSizeOptions]) =>
+    Object.entries(fontSizeOptions).map(([fontSizeLevel, fontSizeValue]) => ({
+      name: `${fontSizeName} ${fontSizeLevel}`,
+      value: fontSizeValue
+    })
+  )).forEach(({ name, value}) => {
+    registerToken({
+      type: "font-size",
+      name,
+      value
+    })
+  })
+
+  Object.entries(semanticTokens.fontWeights)
+  .flatMap(([fontWeightName, fontWeightOptions]) =>
+    Object.entries(fontWeightOptions).map(([fontWeightLevel, fontWeightValue]) => ({
+      name: `${fontWeightName} ${fontWeightLevel}`,
+      value: fontWeightValue
+    })
+  )).forEach(({ name, value}) => {
+    registerToken({
+      // Samuel says "Use line-height for font-weight" for now
+      type: "line-height",
+      name,
+      value
+    })
+  })
+
+  // Add radii, font-weight, sizes, borders to Plasmic, 
+  // Add opacity to TokenStudio, 
+
+  Object.entries(semanticTokens.radii)
+  .flatMap(([radiiName, radiiOptions]) =>
+    Object.entries(radiiOptions).map(([radiiLevel, radiiValue]) => ({
+      name: `${radiiName} ${radiiLevel}`,
+      value: radiiValue
+    })
+  )).forEach(({ name, value}) => {
+    registerToken({
+      // Samuel says "Use spacing for radii" for now
+      type: "spacing",
+      name,
+      value
+    })
+  })
+
+  Object.entries(semanticTokens.sizes)
+  .flatMap(([sizeName, sizeOptions]) =>
+    Object.entries(sizeOptions).map(([sizeLevel, sizeValue]) => ({
+      name: `${sizeName} ${sizeLevel}`,
+      value: sizeValue
+    })
+  )).forEach(({ name, value}) => {
+    registerToken({
+      // Samuel says "Use spacing for sizes" for now
+      type: "spacing",
+      name,
+      value
+    })
+  })
+
+  Object.entries(semanticTokens.borders)
+  .flatMap(([borderName, borderOptions]) =>
+    Object.entries(borderOptions).map(([borderLevel, borderValue]) => ({
+      name: `${borderName} ${borderLevel}`,
+      value: borderValue
+    })
+  )).forEach(({ name, value}) => {
+    registerToken({
+      // Samuel says "Use spacing for border" for now
+      type: "spacing",
+      name,
+      value
+    })
+  })
 
 
