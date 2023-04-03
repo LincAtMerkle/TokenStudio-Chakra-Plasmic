c01bec1bfc50ad07f0fecc8a2fc9098dc649eed3
Lincoln Mitchell <lincolnmitchell@merkle.com>
2023-04-03 11:35:03 +1000

    AntdConfigProvider issue

--- components/plasmic/plasmic_poc/PlasmicGlobalContextsProvider.tsx
+++ components/plasmic/plasmic_poc/PlasmicGlobalContextsProvider.tsx
@@ -7,7 +7,7 @@
 import * as React from "react";
 import { hasVariant, ensureGlobalVariants } from "@plasmicapp/react-web";
 import { ChakraProvider } from "@chakra-ui/react"; // plasmic-import: W7daNWrClxD/codeComponent
-import { AntdConfigProvider } from "@plasmicpkgs/antd5/skinny/registerConfigProvider"; // plasmic-import: ixWyRxIJ6ze/codeComponent
+// import {   } from "@plasmicpkgs/antd5/skinny/registerConfigProvider"; // plasmic-import: ixWyRxIJ6ze/codeComponent
 
 export interface GlobalContextsProviderProps {
   children?: React.ReactElement;
@@ -44,90 +44,90 @@ export default function GlobalContextsProvider(
           : ("#0000FF" as const)
       }
     >
-      <AntdConfigProvider
-        {...antdConfigProviderProps}
-        borderRadius={
-          antdConfigProviderProps && "borderRadius" in antdConfigProviderProps
-            ? antdConfigProviderProps.borderRadius!
-            : (6 as const)
-        }
-        colorBgBase={
-          antdConfigProviderProps && "colorBgBase" in antdConfigProviderProps
-            ? antdConfigProviderProps.colorBgBase!
-            : ("#ffffff" as const)
-        }
-        colorError={
-          antdConfigProviderProps && "colorError" in antdConfigProviderProps
-            ? antdConfigProviderProps.colorError!
-            : ("#ff4d4f" as const)
-        }
-        colorInfo={
-          antdConfigProviderProps && "colorInfo" in antdConfigProviderProps
-            ? antdConfigProviderProps.colorInfo!
-            : ("#1677ff" as const)
-        }
-        colorPrimary={
-          antdConfigProviderProps && "colorPrimary" in antdConfigProviderProps
-            ? antdConfigProviderProps.colorPrimary!
-            : ("#1677ff" as const)
-        }
-        colorSuccess={
-          antdConfigProviderProps && "colorSuccess" in antdConfigProviderProps
-            ? antdConfigProviderProps.colorSuccess!
-            : ("#52c41a" as const)
-        }
-        colorWarning={
-          antdConfigProviderProps && "colorWarning" in antdConfigProviderProps
-            ? antdConfigProviderProps.colorWarning!
-            : ("#faad14" as const)
-        }
-        controlHeight={
-          antdConfigProviderProps && "controlHeight" in antdConfigProviderProps
-            ? antdConfigProviderProps.controlHeight!
-            : (32 as const)
-        }
-        lineWidth={
-          antdConfigProviderProps && "lineWidth" in antdConfigProviderProps
-            ? antdConfigProviderProps.lineWidth!
-            : (1 as const)
-        }
-        sizeStep={
-          antdConfigProviderProps && "sizeStep" in antdConfigProviderProps
-            ? antdConfigProviderProps.sizeStep!
-            : (4 as const)
-        }
-        sizeUnit={
-          antdConfigProviderProps && "sizeUnit" in antdConfigProviderProps
-            ? antdConfigProviderProps.sizeUnit!
-            : (4 as const)
-        }
-        spacing={
-          antdConfigProviderProps && "spacing" in antdConfigProviderProps
-            ? antdConfigProviderProps.spacing!
-            : undefined
-        }
-        themeStyles={
-          antdConfigProviderProps && "themeStyles" in antdConfigProviderProps
-            ? antdConfigProviderProps.themeStyles!
-            : true
-            ? {
-                fontFamily: "Inter",
-                fontSize: "16px",
-                fontWeight: "400",
-                lineHeight: "1.5",
-                color: "#535353",
-                letterSpacing: "normal"
-              }
-            : undefined
-        }
-        wireframe={
-          antdConfigProviderProps && "wireframe" in antdConfigProviderProps
-            ? antdConfigProviderProps.wireframe!
-            : false
-        }
-      >
-        {children}
-      </AntdConfigProvider>
+      {/* <AntdConfigProvider
+         {...antdConfigProviderProps}
+         borderRadius={
+           antdConfigProviderProps && "borderRadius" in antdConfigProviderProps
+             ? antdConfigProviderProps.borderRadius!
+             : (6 as const)
+         }
+         colorBgBase={
+           antdConfigProviderProps && "colorBgBase" in antdConfigProviderProps
+             ? antdConfigProviderProps.colorBgBase!
+             : ("#ffffff" as const)
+         }
+         colorError={
+           antdConfigProviderProps && "colorError" in antdConfigProviderProps
+             ? antdConfigProviderProps.colorError!
+             : ("#ff4d4f" as const)
+         }
+         colorInfo={
+           antdConfigProviderProps && "colorInfo" in antdConfigProviderProps
+             ? antdConfigProviderProps.colorInfo!
+             : ("#1677ff" as const)
+         }
+         colorPrimary={
+           antdConfigProviderProps && "colorPrimary" in antdConfigProviderProps
+             ? antdConfigProviderProps.colorPrimary!
+             : ("#1677ff" as const)
+         }
+         colorSuccess={
+           antdConfigProviderProps && "colorSuccess" in antdConfigProviderProps
+             ? antdConfigProviderProps.colorSuccess!
+             : ("#52c41a" as const)
+         }
+         colorWarning={
+           antdConfigProviderProps && "colorWarning" in antdConfigProviderProps
+             ? antdConfigProviderProps.colorWarning!
+             : ("#faad14" as const)
+         }
+         controlHeight={
+           antdConfigProviderProps && "controlHeight" in antdConfigProviderProps
+             ? antdConfigProviderProps.controlHeight!
+             : (32 as const)
+         }
+         lineWidth={
+           antdConfigProviderProps && "lineWidth" in antdConfigProviderProps
+             ? antdConfigProviderProps.lineWidth!
+             : (1 as const)
+         }
+         sizeStep={
+           antdConfigProviderProps && "sizeStep" in antdConfigProviderProps
+             ? antdConfigProviderProps.sizeStep!
+             : (4 as const)
+         }
+         sizeUnit={
+           antdConfigProviderProps && "sizeUnit" in antdConfigProviderProps
+             ? antdConfigProviderProps.sizeUnit!
+             : (4 as const)
+         }
+         spacing={
+           antdConfigProviderProps && "spacing" in antdConfigProviderProps
+             ? antdConfigProviderProps.spacing!
+             : undefined
+         }
+         themeStyles={
+           antdConfigProviderProps && "themeStyles" in antdConfigProviderProps
+             ? antdConfigProviderProps.themeStyles!
+             : true
+             ? {
+                 fontFamily: "Inter",
+                 fontSize: "16px",
+                 fontWeight: "400",
+                 lineHeight: "1.5",
+                 color: "#535353",
+                 letterSpacing: "normal"
+               }
+             : undefined
+         }
+         wireframe={
+           antdConfigProviderProps && "wireframe" in antdConfigProviderProps
+             ? antdConfigProviderProps.wireframe!
+             : false
+         }
+        >
+         {children}
+        </AntdConfigProvider> */}
     </ChakraProvider>
   );
 }
