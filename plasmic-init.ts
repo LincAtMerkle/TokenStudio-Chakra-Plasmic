fe58514b5fbacd485daef1399e44759df7c392b8
Lincoln Mitchell <lincolnmitchell@merkle.com>
2023-05-15 09:03:29 +1000

    project id and sync

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,9 +1,9 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
 export const PLASMIC = initPlasmicLoader({
   projects: [
-    { // FocusTokens
-      id: "6VdvnRp9F6ae11p6fdVqu9",  // ID of a project you are using
-      token: "dXcuNHgAOgV8aIA9ubdyZ1Qkqd3uXC0eT9TpYaFoAq0KfXaKK7WcUmXFdUL6AjhijKTaDG5LZtEoRB3w"  // API token for that project
+    { // Plasmic POC (locahost)
+      id: "nV7sRTPBx6ecWzFNNAWeQg",  // ID of a project you are using
+      token: "NIKKvwe5oRoMzc1Ag6Y0edSDYxfGY1n9Aq2D9dqA1tC0nzLJgWADIVPivuIexoJWe1G1klGrrnm1rYErzvt2w"  // API token for that project
     }
   ],
   // Fetches the latest revisions, whether or not they were unpublished!
