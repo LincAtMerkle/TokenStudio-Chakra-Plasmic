d4b86c272bca8b18c82fbdc5c77df654bd92e5bd
Lincoln Mitchell <lincolnmitchell@merkle.com>
2023-06-07 17:19:36 +1000

    push latest

--- components/custom/chakraPro/Application/Authentication/LoginWithCentredForm/register.tsx
+++ components/custom/chakraPro/Application/Authentication/LoginWithCentredForm/register.tsx
@@ -1,27 +1,30 @@
-import { App }  from "./App";
-import {registerComponent } from "@plasmicapp/host";
-
-export function registerLoginWithCentredForm(){
-    registerComponent(App, {
-        name: "LoginWithCentredForm",
-        importPath: "./components/chakra/chakraPro/Application/Authentication/LoginWithCentredForm",
-        props: {
-          title: {
-          type: "string",
-          defaultValue: "xxxxxxxx",
-        },
-        children: {
-          type: 'slot',
-          // defaultValue: [
-          //   {
-          //     type: 'component',
-          //     name: 'Stack',
-          //     props: {
-
-          //     }
-          //   }
-          // ]
-        }
-  },
-});};
-// 
+import { App } from "./App";
+import { registerComponent } from "@plasmicapp/host";
+import {
+  getComponentNameAndImportMeta,
+  getPlasmicComponentName,
+} from "../../../../../chakra/utils";
+export function registerLoginWithCentredForm() {
+  registerComponent(App, {
+    name: "LoginWithCentredForm",
+    importPath:
+      "./components/chakra/chakraPro/Application/Authentication/LoginWithCentredForm",
+    props: {
+      children: {
+        type: "slot",
+        defaultValue: [
+          {
+            type: "component",
+            name: getPlasmicComponentName("Stack"),
+            props: {
+              children: {
+                type: "component",
+                name: getPlasmicComponentName("Stack"),
+              },
+            },
+          },
+        ],
+      },
+    },
+  });
+}
