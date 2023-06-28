93315406be6242dc1b0077b317211e27cc28f3a6
Lincoln Mitchell <lincolnmitchell@merkle.com>
2023-06-28 15:18:13 +1000

    Added all other componets from old arhitecture and started edited the css

--- src/components/custom-select/custom-select.ts
+++ src/components/custom-select/custom-select.ts
@@ -1,146 +1,157 @@
-import { getColor, mode, StyleFunctionProps, transparentize } from '@chakra-ui/theme-tools'
-
-const parts = ['field', 'menu', 'option']
-
-const baseStyle = {
+.custom-select__container {
   field: {
-    width: '100%',
-    display: 'inline-flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    transition: 'all 50ms ease',
-  },
+    width: 100%
+;
+    display: var(--inline-flex);
+    align-items: var(--center);
+    justify-content: var(--space-between);
+    transition: var(--all 50ms ease);
+  }
+
   option: {
-    display: 'flex',
-    alignItems: 'center',
-    cursor: 'pointer',
+    display: var(--flex);
+    align-items: var(--center);
+    cursor: var(--pointer);
     _disabled: {
       opacity: 0.4,
-      cursor: 'not-allowed',
-    },
-  },
+      cursor: var(--not-allowed);
+    }
+
+  }
+
   menu: {
-    minW: '3xs',
-  },
+    min-width: var(--3xs);
+  }
+
 }
 
-const variants = {
+
   outline: (props: StyleFunctionProps) => ({
     menu: {
       bg: mode('white', 'gray.800')(props),
-      boxShadow: mode('sm', 'sm-dark')(props),
-      color: 'inherit',
-      minW: '3xs',
-      py: '2',
-      borderRadius: 'md',
-      borderWidth: '1px',
-    },
+      box-shadow: mode('sm', 'sm-dark')(props),
+      color: var(--inherit);
+      min-width: var(--3xs);
+      padding-left: var(--2);
+      padding-right: var(--2);
+  
+      border-radius: var(--md);
+      border-width: var(--1px);
+    }
+
     option: {
       _selected: {
         bg: mode('gray.100', 'gray.700')(props),
-      },
-    },
+      }
+
+    }
+
     field: {
-      border: '1px solid',
-      borderColor: 'inherit',
+      border: var(--1px solid);
+      border-color: var(--inherit);
       bg: mode('white', 'gray.800')(props),
 
       _hover: {
-        borderColor: mode('gray.300', 'whiteAlpha.400')(props),
-      },
+        border-color: mode('gray.300', 'whiteAlpha.400')(props),
+      }
+
       _disabled: {
         opacity: 0.4,
-        cursor: 'not-allowed',
-        borderColor: 'inherit',
-      },
+        cursor: var(--not-allowed);
+        border-color: var(--inherit);
+      }
+
       _readOnly: {
-        boxShadow: 'none !important',
-        userSelect: 'all',
-      },
+        box-shadow: var(--none !important);
+        userSelect: var(--all);
+      }
+
       _invalid: {
-        borderColor: getColor(props.theme, mode('red.500', 'red.300')(props)),
-        boxShadow: `0 0 0 1px ${getColor(props.theme, mode('red.500', 'red.300')(props))}`,
-      },
+        border-color: getColor(props.theme, mode('red.500', 'red.300')(props)),
+        box-shadow: `0 0 0 1px ${getColor(props.theme, mode('red.500', 'red.300')(props))}`,
+      }
+
       _focus: {
-        borderColor: mode('brand.500', 'brand.200')(props),
-        boxShadow: mode(
+        border-color: mode('brand.500', 'brand.200')(props),
+        box-shadow: mode(
           `0px 0px 0px 1px ${transparentize(`brand.500`, 1.0)(props.theme)}`,
           `0px 0px 0px 1px ${transparentize(`brand.200`, 1.0)(props.theme)}`,
         )(props),
-      },
+      }
+
       _expanded: {
-        borderColor: mode('brand.500', 'brand.200')(props),
-        boxShadow: mode(
+        border-color: mode('brand.500', 'brand.200')(props),
+        box-shadow: mode(
           `0px 0px 0px 1px ${transparentize(`brand.500`, 1.0)(props.theme)}`,
           `0px 0px 0px 1px ${transparentize(`brand.200`, 1.0)(props.theme)}`,
         )(props),
-      },
-    },
+      }
+
+    }
+
   }),
 }
 
-const sizes = {
-  sm: {
-    field: {
-      px: 3,
-      h: 8,
-      fontSize: 'sm',
-      borderRadius: 'sm',
-    },
-    menu: {
-      fontSize: 'sm',
-      borderRadius: 'sm',
-    },
-    option: {
-      px: 3,
-      h: 8,
-      fontSize: 'sm',
-    },
-  },
-  md: {
-    field: {
-      px: 4,
-      h: 10,
-      fontSize: 'md',
-      borderRadius: 'md',
-    },
-    menu: {
-      fontSize: 'md',
-      borderRadius: 'md',
-    },
-    option: {
-      px: 4,
-      h: 10,
-      fontSize: 'md',
-    },
-  },
-  lg: {
-    field: {
-      px: 4,
-      h: 12,
-      fontSize: 'lg',
-      borderRadius: 'md',
-    },
-    menu: {
-      fontSize: 'lg',
-      borderRadius: 'md',
-    },
-    option: {
-      px: 4,
-      h: 12,
-      fontSize: 'lg',
-    },
-  },
-}
 
-export default {
-  parts,
-  baseStyle,
-  variants,
-  sizes,
-  defaultProps: {
-    size: 'md',
-    variant: 'outline',
-    colorScheme: 'brand',
-  },
-}
+.custom-select__sm {
+    field: {
+      px: 3,
+      h: 8,
+      font-size: var(--sm);
+      border-radius: var(--sm);
+    }
+
+    menu: {
+      font-size: var(--sm);
+      border-radius: var(--sm);
+    }
+
+    option: {
+      px: 3,
+      h: 8,
+      font-size: var(--sm);
+    }
+
+  }
+
+.custom-select__md {
+    field: {
+      px: 4,
+      h: 10,
+      font-size: var(--md);
+      border-radius: var(--md);
+    }
+
+    menu: {
+      font-size: var(--md);
+      border-radius: var(--md);
+    }
+
+    option: {
+      px: 4,
+      h: 10,
+      font-size: var(--md);
+    }
+
+  }
+
+.custom-select__lg {
+    field: {
+      px: 4,
+      h: 12,
+      font-size: var(--lg);
+      border-radius: var(--md);
+    }
+
+    menu: {
+      font-size: var(--lg);
+      border-radius: var(--md);
+    }
+
+    option: {
+      px: 4,
+      h: 12,
+      font-size: var(--lg);
+    }
+
+  }
