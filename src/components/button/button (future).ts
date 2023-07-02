39850045530e0e2ec013068869b45c059e909c21
Lincoln Mitchell <lincolnmitchell@merkle.com>
2023-07-02 13:52:22 +1000

    button future example

--- src/components/button/button (future).ts
+++ src/components/button/button (future).ts
@@ -1,169 +1,151 @@
-.xxxxx__container {
-  font-weight: var(--btn-font-weight);
-  text-transform: var(--uppercase);
-  width: var(--auto);
-  border-radius: var(--btn-border-radius);
-  font-family: var(--btn-font-family);
-  border: var(--btn-border-width);
-  border-style: var(--solid)
+import { darken, mode, StyleFunctionProps, transparentize } from '@chakra-ui/theme-tools'
+
+const baseStyle = {
+  fontWeight: 'btn-font-weight',
+  textTransform: 'uppercase',
+  width: 'auto',
+  borderRadius: 'btn-border-radius',
+  fontFamily: 'btn-font-family',
+  border: 'btn-border-width',
+  borderStyle: 'solid'
 }
 
-
-.xxxxx__xs {
-    padding-left: var(--btn-xs-padding-left);
-    padding-right: var(--btn-xs-padding-right);
-    height: var(--btn-xs-height);
-    font-size: var(--btn-xs-text-size)
-  }
-
-.xxxxx__sm {
-    padding-left: var(--btn-sm-padding-left);
-    padding-right: var(--btn-sm-padding-right);
-    height: var(--btn-sm-height);
-    font-size: var(--btn-sm-text-size)
-  }
-
-.xxxxx__md {
-    padding-left: var(--btn-md-padding-left);
-    padding-right: var(--btn-md-padding-right);
-    height: var(--btn-md-height);
-    font-size: var(--btn-md-text-size)
-  }
-
-.xxxxx__lg {
-    padding-left: var(--btn-lg-padding-left);
-    padding-right: var(--btn-lg-padding-right);
-    height: var(--btn-lg-height);
-    font-size: var(--btn-lg-text-size)
+const sizes = {
+  xs: {
+    paddingLeft: 'btn-xs-padding-left',
+    paddingRight: 'btn-xs-padding-right',
+    h: 'btn-xs-height',
+    fontSize: 'btn-xs-text-size'
+  },
+  sm: {
+    paddingLeft: 'btn-sm-padding-left',
+    paddingRight: 'btn-sm-padding-right',
+    height: 'btn-sm-height',
+    fontSize: 'btn-sm-text-size'
+  },
+  md: {
+    paddingLeft: 'btn-md-padding-left',
+    paddingRight: 'btn-md-padding-right',
+    height: 'btn-md-height',
+    fontSize: 'btn-md-text-size'
+  },
+  lg: {
+    paddingLeft: 'btn-lg-padding-left',
+    paddingRight: 'btn-lg-padding-right',
+    height: 'btn-lg-height',
+    fontSize: 'btn-lg-text-size'
   }
 }
 
-
+const variants = {
   primary: (props: StyleFunctionProps) =>
     props.theme.components['Button']['variants']['solid']({
-      ...props;
-      variant: var(--solid);
-      colorScheme: var(--gray);
-    });
+      ...props,
+      variant: 'solid',
+      colorScheme: 'gray',
+    }),
   'primary-on-accent': () => ({
-    background: var(--bg-accent);
-    color: var(--brand.600);
-    ._hover { background: var(--brand.100' }
-
-    ._active { background: var(--brand.100' }
-
-  });
+    bg: 'bg-accent',
+    color: 'brand.600',
+    _hover: { bg: 'brand.100' },
+    _active: { bg: 'brand.100' },
+  }),
   secondary: (props: StyleFunctionProps) =>
     props.theme.components['Button']['variants']['outline']({
-      ...props;
-      variant: var(--outline);
-      colorScheme: var(--purple);
-    });
+      ...props,
+      variant: 'outline',
+      colorScheme: 'purple',
+    }),
   'secondary-on-accent': {
-    color: var(--white);
-    border-color: var(--brand.50);
-    border-width: var(--1px);
-    ._hover { background: var(--whiteAlpha.200' }
-
-    ._active { background: var(--whiteAlpha.200' }
-
-  }
-
+    color: 'white',
+    borderColor: 'brand.50',
+    borderWidth: '1px',
+    _hover: { bg: 'whiteAlpha.200' },
+    _active: { bg: 'whiteAlpha.200' },
+  },
   outline: (props: StyleFunctionProps) => ({
-    color: var(--emphasized);
-    /*bg: mode('white', 'gray.800')(props);
-    background: mode('btn-bg', 'gray.800')(props);
-    fg: mode('btn-fg', 'gray.800')(props);
-    ._hover {
-      background: mode(
-        darken('gray.50', 1)(props.theme);
-        transparentize('gray.700', 0.4)(props.theme);
-      )(props);
-    }
-
-    ._checked {
-      background: mode('gray.100', 'gray.700')(props);
-    }
-
-    ._active {
-      background: mode('gray.100', 'gray.700')(props);
-    }
-
-  });
+    color: 'emphasized',
+    // bg: mode('white', 'gray.800')(props),
+    bg: mode('btn-bg', 'gray.800')(props),
+    fg: mode('btn-fg', 'gray.800')(props),
+    _hover: {
+      bg: mode(
+        darken('gray.50', 1)(props.theme),
+        transparentize('gray.700', 0.4)(props.theme),
+      )(props),
+    },
+    _checked: {
+      bg: mode('gray.100', 'gray.700')(props),
+    },
+    _active: {
+      bg: mode('gray.100', 'gray.700')(props),
+    },
+  }),
   ghost: (props: StyleFunctionProps) => ({
-    color: var(--emphasized);
-    ._hover {
-      background: mode(darken('gray.50', 1)(props.theme), darken('gray.700', 4)(props.theme))(props);
-    }
-
-    ._active {
-      background: mode(darken('gray.50', 1)(props.theme), darken('gray.700', 4)(props.theme))(props);
-    }
-
+    color: 'emphasized',
+    _hover: {
+      bg: mode(darken('gray.50', 1)(props.theme), darken('gray.700', 4)(props.theme))(props),
+    },
+    _active: {
+      bg: mode(darken('gray.50', 1)(props.theme), darken('gray.700', 4)(props.theme))(props),
+    },
     _activeLink: {
-      background: mode('gray.100', 'gray.700')(props);
-    }
-
-  });
+      bg: mode('gray.100', 'gray.700')(props),
+    },
+  }),
   'ghost-on-accent': (props: StyleFunctionProps) => ({
-    color: var(--brand.50);
-    ._hover {
-      background: transparentize('brand.500', 0.67)(props.theme);
-    }
-
+    color: 'brand.50',
+    _hover: {
+      bg: transparentize('brand.500', 0.67)(props.theme),
+    },
     _activeLink: {
-      color: var(--white);
-      background: var(--bg-accent-subtle);
-    }
-
-  });
+      color: 'white',
+      bg: 'bg-accent-subtle',
+    },
+  }),
   link: (props: StyleFunctionProps) => {
     if (props.colorScheme === 'gray') {
       return {
-        color: var(--muted);
-        ._hover {
-          text-decoration: var(--none);
-          color: var(--default);
-        }
-
-        ._active {
-          color: var(--default);
-        }
-
+        color: 'muted',
+        _hover: {
+          textDecoration: 'none',
+          color: 'default',
+        },
+        _active: {
+          color: 'default',
+        },
       }
     }
     return {
-      color: mode(`${props.colorScheme}.600`, `${props.colorScheme}.200`)(props);
-      ._hover {
-        color: mode(`${props.colorScheme}.700`, `${props.colorScheme}.300`)(props);
-        text-decoration: var(--none);
-      }
-
-      ._active {
-        color: mode(`${props.colorScheme}.700`, `${props.colorScheme}.300`)(props);
-      }
-
+      color: mode(`${props.colorScheme}.600`, `${props.colorScheme}.200`)(props),
+      _hover: {
+        color: mode(`${props.colorScheme}.700`, `${props.colorScheme}.300`)(props),
+        textDecoration: 'none',
+      },
+      _active: {
+        color: mode(`${props.colorScheme}.700`, `${props.colorScheme}.300`)(props),
+      },
     }
-  }
-
+  },
   'link-on-accent': () => {
     return {
-      padding: 0;
-      height: var(--auto);
-      line-height: var(--normal);
-      verticalAlign: var(--baseline);
-      color: var(--brand.50);
-      ._hover {
-        color: var(--white);
-      }
-
-      ._active {
-        color: var(--white);
-      }
-
+      padding: 0,
+      height: 'auto',
+      lineHeight: 'normal',
+      verticalAlign: 'baseline',
+      color: 'brand.50',
+      _hover: {
+        color: 'white',
+      },
+      _active: {
+        color: 'white',
+      },
     }
-  }
-
+  },
 }
 
-
+export default {
+  baseStyle,
+  variants,
+  sizes,
+}
