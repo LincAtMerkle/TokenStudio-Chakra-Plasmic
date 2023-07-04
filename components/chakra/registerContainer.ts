329d931b7ec9574abc0141e4d814bfd6caf52dc9
Lincoln Mitchell <lincolnmitchell@merkle.com>
2023-07-04 17:55:48 +1000

    truing to add container

--- components/chakra/registerContainer.ts
+++ components/chakra/registerContainer.ts
@@ -1,24 +1,41 @@
-import { BoxProps } from "@chakra-ui/react";
+import { BoxProps, ContainerProps } from "@chakra-ui/react";
 import { CodeComponentMeta } from "@plasmicapp/host/registerComponent";
 import { getComponentNameAndImportMeta } from "./utils";
 
-export const containerMeta: CodeComponentMeta<BoxProps> = {
+export const containerMeta: CodeComponentMeta<ContainerProps> = {
   ...getComponentNameAndImportMeta("Container"),
   props: {
-    py: {
-      type: "number",
-      defaultValue: 12,
+    // py: {
+    //   type: "number",
+    //   defaultValue: 12,
+    // },
+    // px: {
+    //   type: "number",
+    //   defaultValue: 0,
+    // },
+    centerContent:{
+      type: "boolean",
+      defaultValue: false,
     },
-    px: {
-      type: "number",
-      defaultValue: 0,
+    size:{
+      type: "choice",
+      options: ["sm", "md", "lg"],
+      defaultValue: "md",
+    },
+    variant:{
+      type: "choice",
+      options: [
+        "colorful",
+        "bold",
+      ],
+      defaultValue: "colorful",
     },
     children: {
       type: "slot",
       defaultValue: [
         {
           type: 'text',
-          value: 'Click me!'
+          value: 'Container'
         }
       ]
     },
